c735fc7910492d3b1496cd5c386d3298c0a3a573
x-tion <x_tion87@hotmail.com>
2018-02-17 19:10:32 +0700

    - Added completed training.

--- src/Controller/IonicAPIController.php
+++ src/Controller/IonicAPIController.php
@@ -7,67 +7,84 @@
     class IonicAPIController extends AppController
     {
 		
-
-		
-		public function index()
+		public function getData()
 		{
-			// Now an instance of ArticlesTable.
-			$Tblamphurs = TableRegistry::get('Tblamphur');
-			$query = $Tblamphurs->find();
-
+			// Now an instance of DiaryTable.
+			$DiaryTable = TableRegistry::get('Diary');
+			$query = $DiaryTable->find();
 			$this->response->type('json');
 			$this->response->body(json_encode($query));
-			
 			return $this->response;
 		}
 
 				
-		public function add()
+		public function addData()
 		{
-			
-
-			
-			// Now an instance of ArticlesTable.
-			$Tblamphurs = TableRegistry::get('Tblamphur');
+			// Now an instance of DiaryTable.
+			$DiaryTable = TableRegistry::get('Diary');
 			$data = json_decode(file_get_contents('php://input'));
-			$Tblamphur = $Tblamphurs->newEntity();
-			$Tblamphur->code = "002";
-			$Tblamphur->name = "Name 002";
-			//$Tblamphurs->save($Tblamphur);
-	
+			$Diary = $DiaryTable->newEntity();
+			$Diary->title = $data->title;
+			$Diary->detail = $data->detail;
+
+			if(isset($data->image64) && !empty($data->image64)){
+				$fileName = mktime(date('H'), date('i'), date('s'), date('m'), date('d'), date('Y')) . ".jpg";
+				$output_file = "uploads/diary/".$fileName;
+				if(file_put_contents($output_file, base64_decode($data->image64))){
+					$Diary->image = $output_file;
+				}
+			}
+
+			$DiaryTable->save($Diary);
+
 			$this->response->type('json');
-			$this->response->body(json_encode( $data ));
+			$this->response->body(json_encode($Diary));
 			return $this->response;
 		}
 
-		public function edit()
+		public function editData()
 		{
-				// Now an instance of ArticlesTable.
-				$Tblamphurs = TableRegistry::get('Tblamphur');
-				$data = json_decode(file_get_contents('php://input'));
-				$Tblamphur = $Tblamphurs->newEntity();
-				$Tblamphur->code = "002";
-				$Tblamphur->name = "Name 002";
-				//$Tblamphurs->save($Tblamphur);
-		
-				$this->response->type('json');
-				$this->response->body(json_encode( $data ));
-				return $this->response;
+			// Now an instance of DiaryTable.
+			$DiaryTable = TableRegistry::get('Diary');
+			$data = json_decode(file_get_contents('php://input'));
+			$Diary = $DiaryTable->newEntity();
+			$Diary->diary_id = $data->diary_id;
+			$Diary->title = $data->title ;
+			$Diary->detail = $data->detail ;
+
+
+
+			if(isset($data->image64) && !empty($data->image64)){
+				$fileName = mktime(date('H'), date('i'), date('s'), date('m'), date('d'), date('Y')) . ".jpg";
+				$output_file = "uploads/diary/".$fileName;
+				if(file_put_contents($output_file, base64_decode($data->image64))){
+					if(!empty($Diary->image)){
+						unlink($Diary->image);
+					}
+					$Diary->image = $output_file;
+				}
+			}
+
+
+			$DiaryTable->save($Diary);
+	
+			$this->response->type('json');
+			$this->response->body(json_encode($Diary));
+			return $this->response;
 		}
 
 
-		public function delete()
+		public function deleteData()
 		{
-				// Now an instance of ArticlesTable.
-				$Tblamphurs = TableRegistry::get('Tblamphur');
-				$data = json_decode(file_get_contents('php://input'));
-				$Tblamphurs->delete($data->id);
+			// Now an instance of DiaryTable.
+			$DiaryTable = TableRegistry::get('Diary');
+			$data = json_decode(file_get_contents('php://input'));
+			$DiaryTable->delete($data->diary_id);
 
-				$this->response->type('json');
-				$this->response->body(json_encode($data));
-				return $this->response;
+			$this->response->type('json');
+			$this->response->body(json_encode($data));
+			return $this->response;
 		}
-
 
 
     }
